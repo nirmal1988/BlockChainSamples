b4b437d7debdd579b2638796615d0d74bf6fe9a4
Nirmal Patel <nirmal.06it37@gmail.com>
2017-07-28 06:36:16 +0000

    changed getAllVehicles function for customer

--- Vehicle/chainCode/cc.go
+++ Vehicle/chainCode/cc.go
@@ -283,7 +283,7 @@ func (t *SimpleChaincode) getAllVehicles(stub  shim.ChaincodeStubInterface, user
 				rab.Vehicles = append(rab.Vehicles, sb.VehicleId +"-"+ sb.ChassisNumber);
 			}
 		} else if user == "" {
-			// return all vehicles for mfr, dealer, service center
+			// return all vehicles for mfr, dealer, service center users
 			rab.Vehicles = append(rab.Vehicles, sb.VehicleId +"-"+ sb.ChassisNumber);
 		}
 	}
