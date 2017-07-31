77340db094bbe7bad548edf1cb992442ffb6d697
Nirmal Patel <nirmal.06it37@gmail.com>
2017-07-31 09:55:30 +0000

    changed param count

--- Vehicle/chainCode/cc.go
+++ Vehicle/chainCode/cc.go
@@ -421,13 +421,37 @@ func (t *SimpleChaincode) updateVehicle(stub  shim.ChaincodeStubInterface, args 
 	}	
 	
 	var tx VehicleTransaction 
-	tx.TType 	= args[1] +" |"+ updateStr
+	
 	tx.WarrantyStartDate	= args[10]
-	tx.WarrantyEndDate	= args[11]
-		
+	tx.WarrantyEndDate	= args[11]		
 	tx.UpdatedBy   	= args[12]
 	tx.UpdatedOn   	= time.Now().Local().String()
 	
+	//parts-13
+	p := strings.Split(args[13], ",")
+	var pr Part
+	var prFound string
+	updateStr += ",Parts: "
+	for i := range p {
+		c := strings.Split(p[i], "-")
+		pr.PartId = c[0]
+		pr.ProductCode = c[1]
+
+		for j := range bch.Parts {
+			if bch.Parts[j].PartId == pr.PartId {
+				prFound = "Y"
+			}
+		}
+
+		if prFound == "Y" {
+			updateStr += "~Updated  Part #"+ pr.PartId			
+		} else{
+			updateStr += "~Added  Part #"+ pr.PartId			
+		}
+		bch.Parts = append(bch.Parts, pr)
+	}
+	
+	tx.TType 	= args[1] +" |"+ updateStr
 	bch.VehicleTransactions = append(bch.VehicleTransactions, tx)
 
 	//Commit updates part to ledger
