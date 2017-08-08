f4f5ce04572c9a5d19d9db7c73f1426e228a62b2
Nirmal Patel <nirmal.06it37@gmail.com>
2017-08-08 17:00:02 +0530

    added getvehiclebychassisnumber

--- Vehicle/chainCode/cc.go
+++ Vehicle/chainCode/cc.go
@@ -509,6 +509,7 @@ func (t *SimpleChaincode) updateVehicle(stub  shim.ChaincodeStubInterface, args 
 		updateStr += ",Date of Delivery to "+ args[9]
 	}
 
+	// create warranty end date, 1 year from start date
 	var tt =time.Now()
 	const shortForm = "2006-Jan-02"
 	tt, _ = time.Parse(shortForm, args[10])
